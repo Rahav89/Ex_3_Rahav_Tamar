872b393c31367b679e41c7bfd7b713293fb10b98
Rahav89 <rahavshema@gmail.com>
2024-02-20 22:51:21 +0200

    עדכון

--- src/FCComps/Register.jsx
+++ src/FCComps/Register.jsx
@@ -11,57 +11,92 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Autocomplete from '@mui/material/Autocomplete';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-
-//פונקציה הבודקת את הולידציה של שם משתמש
-function validateUserName(userName) {
-    // תבנית של אותיות לועזיות בלבד מספרים ותווים מיוחדים
-    const regex = /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/;
-    // אם האורך מתחת ל 60 תווים וגם עומד בתנאי התווים של התבנית true מחזיר
-    return userName.length <= 60 && regex.test(userName);
-}
-
-//פונקציה הבודקת את הולידציה של הסיסמא
-function validatePassword(password) {
-    //בודק שהסיסמא בין 7 ל-12 תווים 
-    if (password.length < 7 || password.length > 12) {
-        return false;
-    }
-    //בודק שהסיסמא מכילה לפחות תו מיוחד אחד, אות גדולה אחת ומספר אחד
-    const specialCharacters = /[!@#$%^&*(),.?":{}|<>]/;
-    const uppercaseLetter = /[A-Z]/;
-    const digit = /[0-9]/;
-    return (
-        specialCharacters.test(password) &&
-        uppercaseLetter.test(password) &&
-        digit.test(password)
-    );
-}
-
-//פונקציה הבודקת האם הסיסמאות שוות 
-const validateVerifyPassword = (formData) => {
-    return formData.password === formData.verifyPassword;
-}
-
-
-//פונקציה הבודקת את הסוג של התמונה 
-const validatePhotoUser = (filesPhotoUser) => {
-    const file = filesPhotoUser[0];
-    const allowedTypes = ['image/jpeg', 'image/jpg'];
-    return (file && allowedTypes.includes(file.type));
-
-}
-
-//פונקציה הבודקת את הולידציה של המייל
-const validateEmail = (email) => {
-    // Basic email validation regex
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
+//  מערך של ערים בישראל
+const countries = [
+    'ירושלים',
+    'תל אביב',
+    'חיפה',
+    'ראשון לציון',
+    'פתח תקווה',
+    'אשדוד',
+    'נתניה',
+    'באר שבע',
+    'בני ברק',
+    'בת ים',
+];
 
 export default function Register() {
+
+    //פונקציה הבודקת את הולידציה של שם משתמש
+    function validateUserName(userName) {
+        // תבנית של אותיות לועזיות בלבד מספרים ותווים מיוחדים
+        const regex = /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/;
+        // אם האורך מתחת ל 60 תווים וגם עומד בתנאי התווים של התבנית true מחזיר
+        return userName.length <= 60 && regex.test(userName);
+    }
+
+    //פונקציה הבודקת את הולידציה של הסיסמא
+    function validatePassword(password) {
+        //בודק שהסיסמא בין 7 ל-12 תווים 
+        if (password.length < 7 || password.length > 12) {
+            return false;
+        }
+        //בודק שהסיסמא מכילה לפחות תו מיוחד אחד, אות גדולה אחת ומספר אחד
+        const specialCharacters = /[!@#$%^&*(),.?":{}|<>]/;
+        const uppercaseLetter = /[A-Z]/;
+        const digit = /[0-9]/;
+        return (
+            specialCharacters.test(password) &&
+            uppercaseLetter.test(password) &&
+            digit.test(password)
+        );
+    }
+
+    //פונקציה הבודקת האם הסיסמאות שוות 
+    const validateVerifyPassword = (formData) => {
+        return formData.password === formData.verifyPassword;
+    }
+
+    // פונקציה המטפלת בצ'אק בוקס של הסיסמא - בכל לחיצה נשנה מאמת לשקר ולהפך
+    const handleCheckboxChange = () => {
+        setShowPassword(!showPassword);
+    };
+
+    //פונקציה הבודקת האם הקלט הוא טקסט בלבד (בשביל שם פרטי ומשפחה בטופס)
+    const validateTextOnly = (value) => {
+        return /^[a-zA-Z ]*$/.test(value);
+    }
+
+    //פונקציה הבודקת את הולידציה של המייל
+    const validateEmail = (email) => {
+        const regex = /^[a-zA-Z.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z-]+\.com$/;
+        return regex.test(email);
+    }
+
+    //פונקציה הבודקת את הולידציה של תאריך לידה
+    const validateDate = (dateOfBirth) => {
+        const dob = new Date(dateOfBirth);
+        const today = new Date();
+        let age = today.getFullYear() - dob.getFullYear();
+        const m = today.getMonth() - dob.getMonth();
+        //בדיקה של החודשים - אם לא היה לו עדיין חודש יומולדת נוריד מהגיל (הפרש בין השנים) שנה
+        if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
+            age--;
+        }
+
+        if (age < 18 || age > 120) {
+            console.log(false, "under 18 or above 120")
+            return false; // התאריך לא הגיוני אם הגיל קטן מ-18 או גדול מ-120
+        }
+        return true;
+    }
+
     //צאק בוקס של צפייה בסיסמא - useState
     const [showPassword, setShowPassword] = React.useState(false);
+
 
     // אובייקט של הטופס - useState 
     const [formData, setFormData] = React.useState({
@@ -74,9 +109,9 @@ export default function Register() {
         allowExtraEmails: false,
         photoUser: null,
         dateUser: '',
-        countryUser: '',
+        cityUser: '',
         streetName: '',
-        numberHome: 0,
+        homeNumber: 0,
     });
 
     //בדיקת שגיאות - useState
@@ -89,15 +124,11 @@ export default function Register() {
         verifyPassword: false,
         photoUser: false,
         dateUser: false,
-        countryUser: false,
+        cityUser: false,
         streetName: false,
-        numberHome: false,
+        homeNumber: false,
     });
 
-    // פונקציה המטפלת בצ'אק בוקס של הסיסמא - בכל לחיצה נשנה מאמת לשקר ולהפך
-    const handleCheckboxChange = () => {
-        setShowPassword(!showPassword);
-    };
 
     //פונקציה המטפלת בכפתור הסדמיט
     const handleSubmit = (event) => {
@@ -105,96 +136,81 @@ export default function Register() {
         const isUserNameValid = validateUserName(formData.userName);
         const isPasswordValid = validatePassword(formData.password);
         const isPasswordMatch = validateVerifyPassword(formData);
+        const isFirstNameValid = validateTextOnly(formData.firstName);
+        const isLastNameValid = validateTextOnly(formData.lastName);
         const isEmailValid = validateEmail(formData.email);
-        const isPhotoValid = validatePhotoUser(formData.photoUser);
+        const isDateValid = validateDate(formData.dateUser);
+        const isStreetValid = /^[א-ת\s]*$/.test(formData.streetName);
+        // const isHomeNumberValid =parseInt(formData.homeNumber)>=0 ;
+        const isHomeNumberValid = /^\d+$/.test(formData.homeNumber);
 
         console.log(formData);
 
-        setFormErrors({
+        setFormErrors((prevErrors) => ({
+            ...prevErrors,
             userName: !isUserNameValid,
             password: !isPasswordValid,
             verifyPassword: !isPasswordMatch,
+            firstName: !isFirstNameValid,
+            lastName: !isLastNameValid,
             email: !isEmailValid,
-            photoUser: !isPhotoValid,
+            dateUser: !isDateValid,
+            streetName: !isStreetValid,
+            homeNumber: !isHomeNumberValid,
 
-        });
+        }));
 
-        if (isEmailValid && isPasswordValid && isUserNameValid && isPasswordMatch && isPhotoValid) {
+        if (isEmailValid && isHomeNumberValid && isPasswordValid && isStreetValid && isUserNameValid && isPasswordMatch && isFirstNameValid && isLastNameValid && isDateValid) {
             console.log('Form is valid. Submitting data:', formData);
         } else {
             console.log('Form has errors. Please fix them.');
         }
     };
 
-
-
-
     const handleChange = (event) => {
         //const name = event.target.name  :דרך קיצור לכתוב כמה פעמים   
         const { name, value, type, checked, files } = event.target;
 
         let val = null;
+
         if (type === 'checkbox') {
             val = checked;
         }
+        //ולידציה של התמונה לאחר בחירת המשתמש את התמונה
         else if (type === 'file') {
-            val = files;
+            const file = files[0]; //הקובץ שהמשתמש בחר להעלות
+            const allowedTypes = ['image/jpeg', 'image/jpg'];
+            setFormErrors((prevErrors) => ({
+                ...prevErrors,
+                photoUser: !(file && allowedTypes.includes(file.type)),
+            }));
+            //הגדרת הערך של התמונה בשביל האובייקט של הסט-סטייט
+            val = file;
         }
+
         else val = value;
 
-        //שמאפיין את כל שדות הטופס (עדכון של האובקייט) useState שמירת הערך שנכתב בשדה ל  
+        //שמאפיין את כל שדות הטופס (עדכון של האובייקט) useState-שמירת הערך שנכתב בשדה ל  
         setFormData((prevData) => ({
             ...prevData,
             [name]: val,
         }));
 
-        // הוספת בדיקה לשם הרחוב
-        if (name === 'streetName') {
-            const isValidStreetName = /^[א-ת\s]*$/.test(value);
+    };
+    const handleInputChange = (event, newInputValue) => {
+        // If the input value is cleared, prevent it from being cleared
+        if (!newInputValue) {
             setFormErrors((prevErrors) => ({
                 ...prevErrors,
-                streetName: !isValidStreetName ? 'יש להזין רק אותיות בעברית' : '',
+                ['cityUser']: true,// Set error to true if the number is negative
             }));
         }
-
-        // If the input type is 'date'
-        if (type === 'date') {
-            const selectedDate = new Date(value);
-            const currentDate = new Date();
-            const minDate = new Date(currentDate);
-            minDate.setFullYear(minDate.getFullYear() - 120);
-            const maxDate = new Date(currentDate);
-            maxDate.setFullYear(maxDate.getFullYear() - 18);
-
-            if (selectedDate < maxDate || selectedDate > minDate) {
-                setFormErrors((prevErrors) => ({
-                    ...prevErrors,
-                    dateUser: 'Invalid date. Age must be between 18 and 120 years old.',
-                }));
-                return;
-            } else {
-                setFormErrors((prevErrors) => ({
-                    ...prevErrors,
-                    dateUser: '',
-                }));
-            }
-        }
-
-        // If the input type is 'number' and name is 'numberHome'
-        if (type === 'number' && name === 'numberHome') {
-            // Check if the value is a positive number
-            if (parseInt(value) >= 0) {
-                setFormErrors((prevErrors) => ({
-                    ...prevErrors,
-                    [name]: false, // Clear any existing error for this field
-                }));
-            } else {
-                setFormErrors((prevErrors) => ({
-                    ...prevErrors,
-                    [name]: true, // Set error to true if the number is negative
-                }));
-            }
-        }
+    };
+    const handleChange_Autocomplete_City = (event, newValue) => {
+        setFormData(prevData => ({
+            ...prevData,
+            ['cityUser']: newValue,
+        }));
     };
 
     return (
@@ -260,31 +276,28 @@ export default function Register() {
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
+                                    type='file'
                                     required
                                     fullWidth
                                     error={formErrors.photoUser}
                                     helperText={formErrors.photoUser ? 'Only JPG or JPEG files are allowed.' : ""}
                                     name="photoUser"
                                     label="Photo User"
-                                    type='file'
                                     id="photoUser"
                                     onChange={handleChange}
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6}>
                                 <TextField
                                     autoComplete="given-name"
-                                    name="firstName"
                                     required
                                     fullWidth
+                                    name="firstName"
                                     id="firstName"
                                     label="First Name"
-                                    autoFocus
-                                    value={formData.firstName}
                                     onChange={handleChange}
+                                    error={formErrors.firstName}
+                                    helperText={formErrors.firstName ? 'Only text are allowed.' : ""}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6}>
@@ -295,9 +308,9 @@ export default function Register() {
                                     label="Last Name"
                                     name="lastName"
                                     autoComplete="family-name"
-                                    value={formData.lastName}
+                                    error={formErrors.lastName}
+                                    helperText={formErrors.lastName ? 'Only text are allowed.' : ""}
                                     onChange={handleChange}
-
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -315,37 +328,45 @@ export default function Register() {
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
+                                    type='date'
                                     required
                                     fullWidth
                                     error={formErrors.dateUser}
-                                    helperText={formErrors.dateUser}
+                                    helperText={formErrors.dateUser ? 'Invalid date. Age must be between 18 and 120 years old.' : ''}
                                     id="dateUser"
                                     label="Date"
-                                    type='date'
                                     name="dateUser"
                                     autoComplete="dateUser"
                                     onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    error={formErrors.countryUser}
-                                    helperText={formErrors.countryUser}
-                                    id="countryUser"
-                                    label="Country User"
-                                    type='text'
-                                    name="countryUser"
-                                    autoComplete="countryUser"
-                                    onChange={handleChange}
+                                <Autocomplete
+                                    options={countries} // הגדרת האפשרויות לרשימה של מדינות
+                                    getOptionLabel={(option) => option} // כיצד להציג את כל אפשרות
+                                    onChange={handleChange_Autocomplete_City}//מטפל ברשימה הנגללת 
+                                    onInputChange={handleInputChange}//מטפל באיקס של התיבה
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            required
+                                            fullWidth
+                                            error={!!formErrors.cityUser}
+                                            helperText={formErrors.cityUser ? 'You must choose a city' : ''}
+                                            label="City User"
+                                            type="text"
+                                            name="cityUser"
+                                            autoComplete="cityUser"
+                                        />
+                                    )}
                                 />
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
                                     required
                                     fullWidth
-                                    helperText={formErrors.streetName}
+                                    error={formErrors.streetName}
+                                    helperText={formErrors.streetName ? 'Please write street name in Hebrew only.' : ''}
                                     id="streetName"
                                     label="Street Name"
                                     type='text'
@@ -358,13 +379,13 @@ export default function Register() {
                                 <TextField
                                     required
                                     fullWidth
-                                    id="numberHome"
+                                    id="homeNumber"
                                     label="Number Home"
                                     type='number'
-                                    name="numberHome"
-                                    autoComplete="numberHome"
-                                    helperText={formErrors.numberHome ? 'Please enter a positive number' : ''}
-                                    error={formErrors.numberHome}
+                                    name="homeNumber"
+                                    autoComplete="homeNumber"
+                                    helperText={formErrors.homeNumber ? 'Please enter a positive number' : ''}
+                                    error={formErrors.homeNumber}
                                     onChange={handleChange}
                                     inputProps={{
                                         min: "0" // מגביל את הערך המינימלי לספרה אחת
